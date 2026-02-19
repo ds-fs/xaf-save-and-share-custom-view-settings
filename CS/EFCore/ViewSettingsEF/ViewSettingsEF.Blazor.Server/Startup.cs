cf54e8e55c0e7b8e9fe75b11fe43d5cf33e83818
Dirk Scholze <dirk.scholze@fischersoftware.de>
2026-02-19 12:00:42 +0100

    update DX 25.2.4

--- CS/EFCore/ViewSettingsEF/ViewSettingsEF.Blazor.Server/Startup.cs
+++ CS/EFCore/ViewSettingsEF/ViewSettingsEF.Blazor.Server/Startup.cs
@@ -96,6 +96,7 @@ public class Startup {
         app.UseRouting();
         app.UseAuthentication();
         app.UseAuthorization();
+        app.UseAntiforgery();
         app.UseXaf();
         app.UseEndpoints(endpoints => {
             endpoints.MapXafEndpoints();
